a120c89095847accfde775bb616fbea04891d586
Dimensioni Nascoste <dimensioninascoste@gmail.com>
2023-09-02 11:11:18 +0200

    Tab 1 modifiche di prova

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,4 +1,66 @@
 <template>
+  <ion-item>
+    <ion-label>Basic Item</ion-label>
+  </ion-item>
+
+  <ion-item>
+    <ion-label>
+      Multi-line text that should ellipsis when it is too long to fit on one line. Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit.
+    </ion-label>
+  </ion-item>
+
+  <ion-item>
+    <ion-label class="ion-text-wrap">
+      Multi-line text that should wrap when it is too long to fit on one line. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit.
+    </ion-label>
+  </ion-item>
+
+  <ion-item>
+    <ion-label>
+      <h1>H1 Heading</h1>
+      <p>Paragraph</p>
+    </ion-label>
+  </ion-item>
+
+  <ion-item>
+    <ion-label>
+      <h2>H2 Heading</h2>
+      <p>Paragraph</p>
+    </ion-label>
+  </ion-item>
+
+  <ion-item>
+    <ion-label>
+      <h3>H3 Heading</h3>
+      <p>Paragraph</p>
+    </ion-label>
+  </ion-item>
+
+  <ion-card>
+    <ion-card-header>
+      <ion-card-title>Card Title</ion-card-title>
+      <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+    </ion-card-header>
+
+    <ion-card-content>
+      Here's a small text description for the card content. Nothing more, nothing less.
+    </ion-card-content>
+  </ion-card>
+</template>
+
+<script lang="ts">
+  import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem, IonLabel} from '@ionic/vue';
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    components: { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem, IonLabel },
+  });
+</script>
+
+
+<!-- <template>
   <ion-page>
     <ion-header>
       <ion-toolbar>
@@ -21,3 +83,4 @@
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
 </script>
+ -->
