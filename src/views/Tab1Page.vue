39344b91bcc565d21619af37593866996d11e347
mysteryfy-vue-0 <dimensioninascoste@gmail.com>
2023-09-06 18:06:36 +0200

    inserita localStorage per il controllo dell'email

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -15,6 +15,12 @@
                 <!-- <strong>{{ languageCode }}</strong> -->
                 <h1 v-t="'title'"></h1>
                 <p v-t="'description'"></p>
+                <p v-if="currentUser">Bella!</p>
+                <form @submit.prevent="saveData()">
+                    <input type="text" placeholder="name"  v-model="user.name">
+                    <input type="email" placeholder="email" v-model="user.email">
+                    <button type="submit">Save</button>
+                </form>
             </div>
         </ion-content>
     </ion-page>
@@ -27,12 +33,13 @@ import {
     IonPage,
     IonTitle,
     IonToolbar,
-    modalController
+    //modalController
 } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import { key } from 'ionicons/icons';
+//import { defineComponent } from 'vue';
 
-export default defineComponent({
-    name: 'Home',
+export default{
+        name: 'Home',
         components: {
             IonContent,
             IonHeader,
@@ -40,11 +47,32 @@ export default defineComponent({
             IonTitle,
             IonToolbar,
         },
-    methods: {
-      changeLanguage($event: any) {
-            this.$i18n.locale = $event.detail.value;
-            console.log("valore di changeLanguage:", this.$i18n);
-          } 
-    }
-});
+      data(){
+        return{
+          user:{
+             name:'',
+             email:''
+          }
+        }
+      },
+      methods: {
+        saveData(){
+            localStorage.setItem('loggedUser', JSON.stringify(this.user))
+        }
+      },
+      mounted() {
+        var currentUser = JSON.parse(localStorage.getItem('loggedUser'));
+        console.log(currentUser);
+      }
+}
+
+// export default defineComponent({
+
+//     methods: {
+//       changeLanguage($event: any) {
+//             this.$i18n.locale = $event.detail.value;
+//             console.log("valore di changeLanguage:", this.$i18n);
+//           } 
+//     }
+// });
 </script>
