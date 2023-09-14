6f1ef8ed9d0d2c5a85e159ea2fb30277a83dd5b3
mysteryfy-vue-0 <dimensioninascoste@gmail.com>
2023-09-14 17:55:00 +0200

    tabbar compare dopo il login

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -2,19 +2,19 @@
   <ion-page>
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
-      <ion-tab-bar slot="bottom">
+      <ion-tab-bar slot="bottom" v-if="loginLocalStorage === 'dashboard'">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
           <ion-icon aria-hidden="true" :icon="homeOutline" />
           <!-- <ion-icon aria-hidden="true" src="./src/icons/crime-scene.svg" /> -->
           <ion-label v-t="'homeHome'"></ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab2" v-if="loginLocalStorage === 'dashboard'" href="/tabs/tab2">
+        <ion-tab-button tab="tab2" href="/tabs/tab2">
           <ion-icon aria-hidden="true" :icon="fingerPrintSharp" />
           <ion-label v-t="'catalogHome'"></ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab3" v-if="loginLocalStorage === 'dashboard'" href="/tabs/tab3">
+        <ion-tab-button tab="tab3" href="/tabs/tab3">
           <ion-icon aria-hidden="true" :icon="personSharp" />
           <ion-label v-t="'profileHome'"></ion-label>
         </ion-tab-button>
