9969605f0c39c23cd1ba4085048f0eb1b7973980
Dimensioni Nascoste <dimensioninascoste@gmail.com>
2023-09-17 19:08:23 +0200

    dashboard e galleria

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Profile</ion-title>
+        <ion-title>Mysteryfy</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true" class="ion-padding">
@@ -16,10 +16,31 @@
       <div>
         <h1 v-t="'profileTitle'"></h1>
       </div>
+      
+      <ion-grid>
+        <ion-row class="ion-justify-content-center">
+          <ion-col size="4">
+                <ion-avatar slot="start">
+                  <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+                </ion-avatar>
+          </ion-col>
+          <ion-col size="8">
+            <ion-label style="height: 50%">
+                  <h1>Miss Marple</h1>
+                  <p>email: {{ email }}</p>
+                  <p>1969</p>
+                </ion-label>
+          </ion-col>
+        </ion-row>
+        <ion-row class="ion-justify-content-center" style="height: 50%">
+          <ion-col size="3"> 1 </ion-col>
+          <ion-col size="3"> 2 </ion-col>
+        </ion-row>
+      </ion-grid>
+
       <ion-list>
         <ion-item id="locale-changer">
           <ion-select v-model="$i18n.locale" label="Language" placeholder="choose language">
-            <!-- <ion-select-option v-for="(lang, i) in langs" :key="'Lang${i}'" :value="lang">{{ lang }}</ion-select-option> -->
             <ion-select-option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ locale }}</ion-select-option>
           </ion-select>
         </ion-item>
@@ -35,13 +56,20 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
   IonList,
   IonItem,
+  IonThumbnail,
+  IonLabel,
+  IonAvatar,
   IonSelect,
   IonSelectOption
 } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import { Preferences } from '@capacitor/preferences';
 
 export default defineComponent({
   components: {
@@ -50,8 +78,14 @@ export default defineComponent({
     IonToolbar,
     IonTitle,
     IonContent,
+    IonGrid,
+    IonRow,
+    IonCol,
     IonList,
     IonItem,
+    IonThumbnail,
+    IonLabel,
+    IonAvatar,
     IonSelect,
     IonSelectOption
   },
@@ -59,5 +93,20 @@ export default defineComponent({
   data () {
     return { names: ['italian', 'english', 'german'] }
   },
+  methods: {
+
+  },
+  setup() {
+    const email = ref('');
+    async () => {
+      const { value } = await Preferences.get({ key: 'userEmail' });
+        if(value) {
+          email.value = value
+        }
+      }
+    return {
+      email
+    }
+  }
 });
 </script>
--- src/views/Tab3Page.vue
+++ src/views/Tab3Page.vue
@@ -2,13 +2,13 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Profile</ion-title>
+        <ion-title>Library of Cases</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true" class="ion-padding">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Info and help</ion-title>
+          <ion-title size="large">Catalog</ion-title>
         </ion-toolbar>
       </ion-header>
 
@@ -18,19 +18,26 @@
       </div>
 
       <ion-card>
-        <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+        <img alt="The Portrait of the Death" src="https://www.mysteryfy.com/wp-content/uploads/2021/10/Amiltoni-Villa-300x200.jpg" />
         <ion-card-header>
-          <ion-card-title>Card Title</ion-card-title>
-          <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+          <ion-card-title color="secondary">
+            <h2>Il Ritratto della Morte</h2></ion-card-title>
+            <div class="ion-align-self-end">
+              <ion-chip color="warning" style="width: 5rem;">
+                <ion-icon :icon="gift"></ion-icon>
+                <ion-label>Free!</ion-label>
+              </ion-chip>
+            </div>
         </ion-card-header>
 
         <ion-card-content>
-          Here's a small text description for the card content. Nothing more, nothing less.
+          Un'antica maledizione riemerge dal passato portando con sé una scia di omicidi. Fantasmi o un oscuro piano di uno spietato assassino?
         </ion-card-content>
+        <ion-row class="ion-justify-content-center">
+          <ion-button @click="openAdventure('https:www.mysteryfy.com')">Gioca</ion-button>
+        </ion-row>
       </ion-card>
-      
-
-
+    
     </ion-content>
   </ion-page>
 </template>
@@ -46,25 +53,47 @@ import {
   IonCardHeader,
   IonCardTitle,
   IonCardSubtitle,
-  IonCardContent
-
+  IonCardContent,
+  IonRow,
+  IonButton,
+  IonChip,
+  IonAvatar,
+  IonLabel,
+  IonIcon
 } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+//import ExploreContainer from '@/components/ExploreContainer.vue';
+import { gift } from 'ionicons/icons';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   components: {
     IonPage,
-IonHeader,
-IonToolbar,
-IonTitle,
-IonContent,
-IonCard,
-IonCardHeader,
-IonCardTitle,
-IonCardSubtitle,
-IonCardContent
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonRow,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardContent,
+    IonButton,
+    IonChip,
+    IonAvatar,
+    IonLabel,
+    IonIcon
   },
+  setup(){
+    return {
+      gift
+    }
+  },
+  methods: {
+    openAdventure(adventure: string) {
+      window.location.href = adventure
+    }
+  }
 });
 
 </script>
