af91085aa3c7393cf8cdd93a2046d876379f120a
Dimensioni Nascoste <dimensioninascoste@gmail.com>
2023-09-20 08:49:32 +0200

    info e legal

---
+++ src/components/Faq.vue
@@ -0,0 +1,20 @@
+<template>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Page One</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <h1>Page One</h1>
+      <ion-back-button></ion-back-button>
+    </ion-content>
+  </template>
+  
+  <script lang="ts">
+    import { IonHeader, IonTitle, IonToolbar, IonContent, IonNavLink, IonButton } from '@ionic/vue';
+  
+    export default {
+      components: { IonHeader, IonTitle, IonToolbar, IonContent, IonNavLink, IonButton },
+
+    };
+  </script>
---
+++ src/components/Terms.vue
@@ -0,0 +1,20 @@
+<template>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Terms and conditions</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <h1>Terms and conditions</h1>
+      <ion-back-button></ion-back-button>
+      <h1>Terms and conditions</h1>
+    </ion-content>
+  </template>
+  
+  <script lang="ts">
+    import { IonHeader, IonTitle, IonToolbar, IonContent, IonNavLink, IonButton } from '@ionic/vue';
+  
+    export default {
+      components: { IonHeader, IonTitle, IonToolbar, IonContent, IonNavLink, IonButton },
+    };
+  </script>
