95be25e79f6346d7995cc54383833210d8196aff
mysteryfy-vue-0 <dimensioninascoste@gmail.com>
2023-10-13 18:43:59 +0200

    add vue-cli pwa

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ defineCustomElements(window);
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+import './registerServiceWorker';
 
 import { IonicVue } from '@ionic/vue';
 
