3e32d114ffaf0260bac73d7b4191626170135901
Dimensioni Nascoste <dimensioninascoste@gmail.com>
2023-10-14 11:21:48 +0200

    aggiornamento nome e permessi a 764

---
+++ src/components/Globals.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+import { ref} from 'vue';
+import { modalController } from '@ionic/vue';
+
+import Modal from '@/components/Login.vue';
+
+export const loginLocalStorage = ref();
+export const isAuth = ref(true); //set true when developing: false when in productivity
+
+export const openLoginModal = await modalController.create({
+      component: Modal,
+      backdropDismiss: false
+    });
+
+</script>
