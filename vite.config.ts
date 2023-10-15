885ea3a1eeb5feda206639704b1a986ddc973255
Dimensioni Nascoste <dimensioninascoste@gmail.com>
2023-10-15 12:05:47 +0200

    vite-plugin-top-level-await

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,23 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     legacy(),
+    topLevelAwait({
+      promiseExportName: "__tla",
+      promiseImportName: i => `__tla_${i}`
+    }),
     VitePWA({ registerType: 'autoUpdate' })
   ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
-  build: {
-    target: 'esnext'
   }
 /*   test: {
     globals: true,
